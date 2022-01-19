e513211f8e61c5e043e1aec855a04f76b48ee1c3
Aleksey Stepin <diver911@yandex.ru>
2022-01-19 05:42:55 +0300

    adding tests

---
+++ src/test/java/ru/netology/wed/CardDeliveryTest.java
@@ -0,0 +1,33 @@
+package ru.netology.wed;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    @Test
+    public void shouldBookingACard() {
+
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String planningDate = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='date']  input").setValue(planningDate);
+        $("[data-test-id = 'name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79161234455");
+        $("[data-test-id= 'agreement']").click();
+        $(".grid-col button[role='button']").click();
+        $("[data-test-id=notification]").shouldHave(Condition.text("Успешно!" +
+                " Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
+
+    }
+
+}
